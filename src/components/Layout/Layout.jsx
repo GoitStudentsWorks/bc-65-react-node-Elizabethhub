c16187926b4d590e679e2b2b5886fcfdf37da565
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2024-03-13 10:43:54 +0200

    add global style, add fonts, add container(with media query)

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header.jsx';
+import { Container } from './Layout.styled.js';
 
 const Layout = () => {
   return (
     <>
-      <Header />
-      <Outlet />
+      <header>
+        <Container>
+          <Header />
+        </Container>
+      </header>
+      <main>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
     </>
   );
 };
