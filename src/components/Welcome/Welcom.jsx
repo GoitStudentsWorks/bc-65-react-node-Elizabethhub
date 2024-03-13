b4496928d639578f791262b27e3ba434ecf0fe0b
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-03-13 15:33:04 +0200

    Фікс + велком

--- src/components/Welcome/Welcom.jsx
+++ src/components/Welcome/Welcom.jsx
@@ -1,13 +1,38 @@
-import { DivImgContainerWelcome } from './WelcomStyled';
+import SvgHabit from '../../images/svg/svgWelcome/SvgHabit';
+import SvgPersonal from '../../images/svg/svgWelcome/SvgPersonal';
+import SvgView from '../../images/svg/svgWelcome/SvgView';
+import { DivImgContainerWelcome, WelcomeDivCantainer } from './WelcomStyled';
 
 const Welcom = () => {
   return (
     <>
       <DivImgContainerWelcome />
 
-      <div>
-        <h1>Велком</h1>
-      </div>
+      <WelcomeDivCantainer>
+        <div>
+          <h1>Water consumption tracker</h1>
+          <h2>Record daily water intake and track</h2>
+          <h3>Tracker Benefits</h3>
+          <ul>
+            <li>
+              <SvgHabit />
+              <span>Habit drive</span>
+            </li>
+            <li>
+              <SvgView />
+              <span>View statistics</span>
+            </li>
+            <li>
+              <SvgPersonal />
+              <span>Personal rate setting</span>
+            </li>
+            <button>Try tracker</button>
+          </ul>
+        </div>
+        <div>
+          <h1>Право</h1>
+        </div>
+      </WelcomeDivCantainer>
     </>
   );
 };
