e2b8a210e1460940c5b99aa8cc5b681e496c4f1c
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-03-13 13:01:06 +0200

    задній фон, для всіх пристроїв home

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header.jsx';
-import { Container } from './Layout.styled.js';
+import { Container, DivImgContainer } from './Layout.styled.js';
 
 const Layout = () => {
   return (
     <>
+      <DivImgContainer />
+
       <header>
         <Container>
           <Header />
--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import backgroundImageMobail from '../../images/backgroundHome/HomescreenTrackerowaterHovers.png';
+import backgroundImageTablet from '../../images/backgroundHome/HomescreenTrackerofwaterhoversTablet.jpg';
+import backgroundImageDesktop from '../../images/backgroundHome/HomescreenTrackerofwaterhoversDesktop.jpg';
 
 export const Container = styled.div`
   max-width: 100%;
@@ -16,5 +19,30 @@ export const Container = styled.div`
   @media only screen and (min-width: 1440px) {
     max-width: 1440px;
     padding: 0 112px;
+  }
+`;
+export const DivImgContainer = styled.div`
+  z-index: -1;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  @media only screen and (min-width: 1280px) {
+    background-image: url(${backgroundImageDesktop});
+  }
+
+  @media only screen and (min-width: 768px) {
+    background-image: url(${backgroundImageTablet});
+  }
+  @media only screen and (max-width: 767px) {
+    background-image: url(${backgroundImageMobail});
   }
 `;
