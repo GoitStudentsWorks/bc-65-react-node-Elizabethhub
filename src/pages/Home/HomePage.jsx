796eff2b69d8e228bea2fcd8bf3d67d4fa14e302
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-03-14 19:54:58 +0200

    Update HomePage.jsx

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,12 @@
 import DailyNorma from '../../components/DailyNorma/DailyNorma';
-import { DivImgContainer } from '../../components/Layout/Layout.styled';
+
 import WaterCalendar from '../../components/WaterCalendar/WaterCalendar';
 import { HomeContauner } from './HomePageStyled';
 
 const HomePage = () => {
   return (
     <>
-      <DivImgContainer />
+      {/* <DivImgContainer /> */}
       <HomeContauner>
         <DailyNorma />
         <WaterCalendar />
