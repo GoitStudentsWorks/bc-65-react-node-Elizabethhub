196d7de83c5663e116b3874f8d51e97c2308c9da
ElizabethHub <bagiraliza21@gmail.com>
2024-03-14 20:12:05 +0200

    bottle

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,21 +1,17 @@
 import DailyEdit from './DailyEdit';
 import WaterRatio from './WaterRatio';
 import bottleMobile from '../../images/dailyBottle/bottleMobile.png';
-import botleMain from '../../images/dailyBottle/botleMain.png';
+import botleMain from '../../images/dailyBottle/BotleMain.png';
 import { BottleImage, Wrp } from './WaterRatio.styled';
 
 const DailyNorma = () => {
-
   return (
     <>
-
       <Wrp>
-          <DailyEdit />
+        <DailyEdit />
         <img src={bottleMobile} alt="bottle-background" />
         <WaterRatio />
       </Wrp>
-    
-
     </>
   );
 };
