ffb52524fecbd871343cfd81804ecdacd2a3c6a6
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-03-16 11:37:26 +0200

    fix HeaderModalButton

---
+++ src/images/svg/svgheader/HeaderButtonRotateSVG.jsx
@@ -0,0 +1,17 @@
+const HeaderButtonRotateSVG = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={11}
+    height={7}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#407BFF"
+      fillRule="evenodd"
+      d="M5.16.146a.5.5 0 0 1 .706 0l5 5a.5.5 0 0 1-.707.707L5.512 1.206.866 5.853a.5.5 0 1 1-.707-.707l5-5Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default HeaderButtonRotateSVG;
