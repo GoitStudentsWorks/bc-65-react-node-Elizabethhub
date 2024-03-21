dbdd9663a5617ed0ffe382d4dbd0c4f838af9e6e
VictorFil67 <victorfilinovych@gmail.com>
2024-03-21 15:13:30 +0200

    fix api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'https://byte-water-back.onrender.com/api',
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://byte-water-back.onrender.com/api',
 });
