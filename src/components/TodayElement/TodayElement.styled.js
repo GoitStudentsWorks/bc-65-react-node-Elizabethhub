157c8588c4ce55a0aafb3bf2b5b99ed9b4c0b41a
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-03-25 19:48:01 +0200

    add todey styles

--- src/components/TodayElement/TodayElement.styled.js
+++ src/components/TodayElement/TodayElement.styled.js
@@ -125,4 +125,8 @@ export const EmptyListMessage = styled.h3`
   font-weight: 500;
   font-size: 24px;
   line-height: 1.23;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
