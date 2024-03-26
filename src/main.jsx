611d2d072c1cad8dfd2b4031f154675cf6030291
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-03-27 00:08:17 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { I18nextProvider } from 'react-i18next';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/byte-my-water-app/">
+  <BrowserRouter basename="/bc-65-react-node-Elizabethhub/">
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <I18nextProvider i18n={i18n}>
